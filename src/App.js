85c8d2d389101f43c76eb747c4f84a21cb3e608d
irishWarlock89 <138416266+irishWarlock89@users.noreply.github.com>
2023-07-03 00:23:07 -0400

    added weather search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+function App() {
+  const apiKey = "fedc6bbcc738251e8d9bea3f6cd50afd";
+  const [weatherData, setWeatherData] = useState([]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (e) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+          setCity("");
+        });
+  };
+
+  return (
+    <div className="container">
+      <input
+        className="input"
+        placeholder="Enter a city..."
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+      />
+      <button className="button" onClick={getWeather}>
+        Get Weather
+      </button>
+      {typeof weatherData.main === "undefined" ? (
+        <div>
+          <p>
+            Welcome to the weather app! Enter a city name to get the current
+            weather!
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      ) : (
+        <div className="weather-data">
+          <p className="city">{weatherData.name}</p>
+          <p className='temp'>{Math.round(weatherData.main.temp)}℉</p>
+          <p className='weather'>{weatherData.weather[0].main}</p>
+        </div>
+      )}
+
+      {weatherData.cod === "404" ? (
+        <p>City not found.</p>
+      ) : (
+        <div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
